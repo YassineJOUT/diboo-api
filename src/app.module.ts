d827cd839391629fb4e3547ea4f5041c3d97de8a
Yassine JOUT <yassinejout@gmail.com>
2020-04-23 18:12:57 +0000

    house keeping

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql'
-import { CatsModule } from './cats/cats.module';
 import { DatabaseModule } from './db/db.module';
 
 @Module({
   imports: [
-    CatsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
       
--- src/interfaces/cat.interface.ts
+++
@@ -1,8 +0,0 @@
-
-import { Document } from 'mongoose'
-
-export interface Cat extends Document{
-    readonly name: String;
-    readonly age: Number;
-    readonly breed: String;
-}
