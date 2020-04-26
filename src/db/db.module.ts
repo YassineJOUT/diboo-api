099d4455b78affc88b58fa8b83510a4fcb35539e
Yassine JOUT <yassinejout@gmail.com>
2020-04-26 17:19:14 +0000

    refactor(Mongoose): Remove warns

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -6,4 +6,4 @@
 // Import the required modules
 import { MongooseModule } from '@nestjs/mongoose'
 //export the database module that connects to mongoDb altas on the cloud
-export const DatabaseModule = MongooseModule.forRoot('mongodb+srv://yassine:zCrWOvp9CPdPHC99@cluster0-jihfc.mongodb.net/diboo?retryWrites=true&w=majority');
+export const DatabaseModule = MongooseModule.forRoot('mongodb+srv://yassine:zCrWOvp9CPdPHC99@cluster0-jihfc.mongodb.net/diboo?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true  });
