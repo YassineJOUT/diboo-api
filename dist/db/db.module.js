f665e4ad45c1dc11aae2529870951a4d3aad22d1
Yassine JOUT <yassinejout@gmail.com>
2020-04-26 17:05:23 +0000

    deploy

--- dist/db/db.module.js
+++ dist/db/db.module.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("@nestjs/mongoose");
-exports.DatabaseModule = mongoose_1.MongooseModule.forRoot('mongodb+srv://yassine:zCrWOvp9CPdPHC99@cluster0-jihfc.mongodb.net/diboo?retryWrites=true&w=majority');
+exports.DatabaseModule = mongoose_1.MongooseModule.forRoot('mongodb+srv://yassine:zCrWOvp9CPdPHC99@cluster0-jihfc.mongodb.net/diboo?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 //# sourceMappingURL=db.module.js.map
