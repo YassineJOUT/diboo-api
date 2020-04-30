7828a8fdcec895ff8da9d319ae2bb47ad5992044
Yassine JOUT <yassinejout@gmail.com>
2020-04-30 14:51:40 +0000

    add config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,9 @@
 import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+config();
 
 export const mongooseModule = MongooseModule.forRoot(
-  'mongodb+srv://yassine:@cluster0-jihfc.mongodb.net/test?retryWrites=true&w=majority',
+  'mongodb+srv://'+process.env.DATABASE_USER+':'+process.env.DATABASE_PASSWORD+'@cluster0-jihfc.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true 
   }
